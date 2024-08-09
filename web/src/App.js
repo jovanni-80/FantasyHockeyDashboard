9fa53b29dedf518a99c712ed14a63f914597ec52
jovanni-80 <jovanni.rodriguez@yahoo.com>
2024-08-09 11:43:32 -0400

    added formik and yup

--- web/src/App.js
+++ web/src/App.js
@@ -1,36 +1,102 @@
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { useState } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import * as formik from 'formik';
+import * as yup from 'yup';
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const { Formik } = formik;
 
-  const increment = () => {
-    setCounter(counter + 1);
-  } 
+  const [key, setKey] = useState("players");
 
-  const decrement = () => {
-    setCounter(counter - 1 < 0 ? 0 : counter - 1);
+  const playerSchema = yup.object().shape({
+    firstName: yup.string().required('First Name is required.').matches(/^[A-Za-z]{0,30}$/),
+    lastName: yup.string().required('Last Name is required.').matches(/^[A-Za-z]{0,30}$/),
+    team: yup.string().required('Team Name is required.'),
+  });
+
+  const playerForm = () => {
+    return(
+      <Formik
+        validationSchema = {playerSchema}
+        onSubmit={console.log}
+        initialValues={{
+          firstName: 'Pavel',
+          lastName: 'Datsyuk',
+          team: 'Red Wings'
+        }}>
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Form noValidate onSubmit = {handleSubmit}>
+          <Form.Group
+          as={Col}
+          md="4"
+          controlId="validationFormkit101"
+          className="position-relative">
+            <Form.Label>email</Form.Label>
+            <Form.Control
+                  type="text"
+                  name="firstName"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  isValid={touched.firstName && !errors.firstName}
+                />
+                <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                md="4"
+                controlId="validationFormik102"
+                className="position-relative"
+              >
+                <Form.Label>Last name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+                isValid={touched.lastName && !errors.lastName}
+              />
+              <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Button type="submit">Submit form</Button>
+        </Form>
+        )}
+      </Formik>
+    );
+  };
+
+  const playerTab = () => {
+    return (
+      <>{playerForm()}</>
+    )
+  }
+
+  const teamTab = () => {
+    return (
+      <div className="h1">
+        Player
+      </div>
+    )
   }
 
   return (
-    <div className="App m-2">
-      <header>
-        { counter }
-      </header>
-      <ButtonGroup aria-label="Counter">
-        <Button
-          variant = "primary"
-          onClick = {increment}>
-          +
-        </Button>
-        <Button
-          variant = "primary"
-          onClick = {decrement}>
-          -
-        </Button>
-      </ButtonGroup>
-    </div>
+  <div>
+    <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-3">
+      <Tab eventKey="players" title="Players">
+        {playerTab()}
+      </Tab>
+      <Tab eventKey="team" title="Team">
+        {teamTab()}
+      </Tab>
+    </Tabs>
+  </div>
   );
 }
 
